a3f60f39e8c243b6dd7e608d0322e8e46b680cac
Janos Gabler <janos.gabler@gmail.com>
2023-09-18 18:05:23 +0200

    Set up quiz infrastructure.

--- src/epp_topics/python_basics/assignment_and_scalar_types/config.py
+++ src/epp_topics/python_basics/assignment_and_scalar_types/config.py
@@ -1,14 +1,8 @@
 """Definitions of source files for the current chapter."""
 
 SITE_CONTENTS = {
-    "public": (
-        "objectives_materials.md",
-        "exercise.ipynb",
-    ),
-    "internal": (
-        "objectives_materials.md",
-        "exercise.ipynb",
-    ),
+    "public": ("objectives_materials.ipynb",),
+    "internal": ("objectives_materials.ipynb",),
     "other": (
         # "existing figures etc.",
     ),
