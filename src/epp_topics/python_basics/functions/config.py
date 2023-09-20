624f9e182c1df19c10a4d74dacf738f244c82d34
Janos Gabler <janos.gabler@gmail.com>
2023-09-20 16:46:35 +0200

    Finish function screencast.

--- src/epp_topics/python_basics/functions/config.py
+++ src/epp_topics/python_basics/functions/config.py
@@ -4,6 +4,7 @@ SITE_CONTENTS = {
     "public": ("objectives_materials.ipynb",),
     "internal": ("objectives_materials.ipynb",),
     "other": (
+        "screencast/public/function_anatomy.png",
         # "existing figures etc.",
     ),
     "built": ("python_basics-functions.pdf",),
