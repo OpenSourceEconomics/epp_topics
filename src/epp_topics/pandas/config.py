f8ce3086ef1d3ec08d5ef1b7e0f63e890f35388b
Janos Gabler <janos.gabler@gmail.com>
2023-09-27 18:06:47 +0200

    Add chapter.

---
+++ src/epp_topics/pandas/config.py
@@ -0,0 +1,40 @@
+"""Definitions of source files for the current chapter."""
+
+import itertools
+
+from epp_topics.pandas.what_is_pandas import (
+    SITE_CONTENTS as WHAT_IS_PANDAS,
+)
+
+TOPICS = [
+    WHAT_IS_PANDAS,
+]
+
+SITE_CONTENTS = {
+    "chapter_title": "Data management with pandas",
+    "public": tuple(
+        itertools.chain(
+            ("content_objectives.md",),
+            *[topic["public"] for topic in TOPICS],
+        ),
+    ),
+    "internal": tuple(
+        itertools.chain(
+            (
+                "internal_overview.md",
+                "content_objectives.md",
+            ),
+            *[topic["internal"] for topic in TOPICS],
+        ),
+    ),
+    "other": tuple(
+        itertools.chain(
+            *[topic["other"] for topic in TOPICS],
+        ),
+    ),
+    "built": tuple(
+        itertools.chain(
+            *[topic["built"] for topic in TOPICS],
+        ),
+    ),
+}
---
+++ src/epp_topics/pandas/what_is_pandas/screencast/slides.md
@@ -0,0 +1,29 @@
+---
+theme: academic
+class: text-center
+highlighter: shiki
+lineNumbers: false
+info: |
+  ## Effective Programming Practices for Economists
+drawings:
+  persist: false
+transition: fade
+title: Effective Programming Practices for Economists
+defaults:
+  layout: center
+---
+
+### Effective Programming Practices for Economists
+
+<br>
+
+# Texts, Typesetting, and Text Data
+
+### Pandas
+
+<br>
+
+
+Janoś Gabler and Hans-Martin von Gaudecker
+
+---
