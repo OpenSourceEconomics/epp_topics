315e6e8493b629629679cc0573793e975b632f94
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2023-09-28 06:18:10 +0200

    WIP.

--- src/epp_topics/config.py
+++ src/epp_topics/config.py
@@ -8,8 +8,6 @@ from pybaum.tree_util import tree_map
 
 # Type aliases
 OrigSourceOrSiteSource = Literal["orig_source", "site_source"]
-PublicOrInternal = Literal["public", "internal"]
-
 
 # Add the chapter names here in the order they should appear in the book
 CHAPTER_NAMES = ["background", "tools", "git", "python_basics", "texts"]
@@ -17,11 +15,7 @@ CHAPTER_NAMES = ["background", "tools", "git", "python_basics", "texts"]
 
 SRC = Path(__file__).parent.resolve()
 SITE_SOURCE_DIR = SRC.parent.parent / "site_source"
-SITE_DIR = {
-    "public": SRC.parent.parent.parent / "effective-programming-practices",
-    "internal": SRC.parent.parent / "site_internal",
-}
-TEST_DIR = SRC.joinpath("..", "..", "tests").resolve()
+SITE_DIR = SRC.parent.parent.parent / "effective-programming-practices"
 
 
 def load_sources(chapter_name):
@@ -50,12 +44,12 @@ def get_chapter_title(chapter_name: str) -> str:
 
 def get_sources_for_chapter(
     chapter_name: str,
-    p_o_i: PublicOrInternal,
     os_o_ss: OrigSourceOrSiteSource,
 ) -> dict:
-    """Get the original sources for a chapter of the public or internal site."""
-    _r = _RAW_SOURCES[chapter_name]["site_contents"][p_o_i]
-    out_dir = SRC if os_o_ss == "orig_source" else SITE_SOURCE_DIR / p_o_i
+    """Get the original sources for a chapter of the site."""
+    _r = _RAW_SOURCES[chapter_name]["site_contents"]
+    breakpoint()
+    out_root = SRC if os_o_ss == "orig_source" else SITE_SOURCE_DIR
     raw = {
         "toc_sources": _r,
         "other": _RAW_SOURCES[chapter_name]["site_contents"].get("other", ()),
@@ -66,6 +60,6 @@ def get_sources_for_chapter(
     # Leave empty chapters out completely
     raw = raw if raw["toc_sources"] else {}
     return tree_map(
-        func=lambda x: out_dir / chapter_name / x,
+        func=lambda x: out_root / chapter_name / x,
         tree=raw,
     )
---
+++ src/epp_topics/task_create_screencasts.py
@@ -0,0 +1,54 @@
+"""Create figure for this subchapter's screencast."""
+
+import shutil
+import subprocess
+from pathlib import Path
+from typing import Annotated
+
+from pytask import Product
+from epp_topics.config import SITE_SOURCE_DIR, SRC
+
+
+def find_orig_screencasts():
+    """Find all files matching screencast pattern, except for template chapter."""
+    return [
+        sc
+        for sc in SRC.glob("**/screencast/slides.md")
+        if sc.parent.parent.parent.name != "chapter_template"
+    ]
+
+
+for orig_screencast in find_orig_screencasts():
+    orig_dir = orig_screencast.parent
+    screencast_dir = SITE_SOURCE_DIR / orig_dir.relative_to(SRC)
+    screencast_md = screencast_dir / orig_screencast.name
+
+    chapter_name = orig_dir.parent.parent.name
+    topic_name = orig_dir.parent.name
+    screencast_pdf = screencast_dir.parent / f"{chapter_name}-{topic_name}.pdf"
+
+    def task_copy_style_css(
+        orig: Path = SRC / "slidev_config" / "style.css",
+        prod: Annotated[Path, Product] = screencast_dir / "style.css",
+    ):
+        """Copy style.css for slidev presentation."""
+        shutil.copy(orig, prod)
+
+    def task_copy_slides_md(
+        orig: Path = orig_screencast,
+        prod: Annotated[Path, Product] = screencast_md,
+    ):
+        """Copy slides.md for slidev presentation."""
+        shutil.copy(orig, prod)
+
+    def task_export_pdf(
+        screencast_md: Path = screencast_md,
+        screencast_pdf: Annotated[Path, Product] = screencast_pdf,
+    ):
+        """Create slidev presentation and export to pdf."""
+        subprocess.run(
+            f"npx slidev export {screencast_md.absolute()}"
+            f" --output {screencast_pdf.absolute()}",
+            shell=True,
+            check=True,
+        )
