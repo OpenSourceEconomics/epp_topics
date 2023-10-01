34374d9511617148392206051e923e0d43350698
Janos Gabler <janos.gabler@gmail.com>
2023-10-01 16:53:26 +0200

    Postpone two screencasts.

--- src/epp_topics/pandas/config.py
+++ src/epp_topics/pandas/config.py
@@ -9,13 +9,11 @@ from epp_topics.pandas.dataframes_and_series import (
 )
 from epp_topics.pandas.datatypes import SITE_CONTENTS as DATATYPES
 from epp_topics.pandas.functional import SITE_CONTENTS as FUNCTIONAL_DATA_MANAGEMENT
-from epp_topics.pandas.groupby import SITE_CONTENTS as GROUPBY
 from epp_topics.pandas.inspecting_and_summarizing import (
     SITE_CONTENTS as INSPECTING_AND_SUMMARIZING,
 )
 from epp_topics.pandas.joins import SITE_CONTENTS as JOINS
 from epp_topics.pandas.loading_and_saving import SITE_CONTENTS as LOADING_AND_SAVING
-from epp_topics.pandas.missings import SITE_CONTENTS as MISSINGS
 from epp_topics.pandas.normal_forms import SITE_CONTENTS as NORMAL_FORMS
 from epp_topics.pandas.selection import SITE_CONTENTS as SELECTION
 from epp_topics.pandas.what_is_pandas import SITE_CONTENTS as WHAT_IS_PANDAS
@@ -28,11 +26,9 @@ TOPICS = [
     COLUMNS_AND_INDICES,
     SELECTION,
     INSPECTING_AND_SUMMARIZING,
-    MISSINGS,
     CREATING_VARIABLES,
     NORMAL_FORMS,
     JOINS,
-    GROUPBY,
     FUNCTIONAL_DATA_MANAGEMENT,
 ]
 SITE_CONTENTS = {
