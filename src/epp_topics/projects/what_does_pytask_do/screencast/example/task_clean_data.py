3a8ad8e6262c49f33419b9d91db7c11cbfdbf55c
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2023-10-05 05:38:42 +0200

    Make pytask example nicer.

--- src/epp_topics/projects/what_does_pytask_do/screencast/example/task_clean_data.py
+++ src/epp_topics/projects/what_does_pytask_do/screencast/example/task_clean_data.py
@@ -5,14 +5,17 @@ import pandas as pd
 BLD = Path(__file__).parent / "bld"
 
 
-def task_clean_data(data=Path("gapminder.arrow"), produces=BLD / "data.pkl"):
-    data = pd.read_feather(data)
-    data = data.rename(
+def task_clean_data(raw_file=Path("gapminder.arrow"), produces=BLD / "data.pkl"):
+    raw = pd.read_feather(raw_file)
+    clean = _clean_data(raw)
+    clean.to_pickle(produces)
+
+
+def _clean_data(raw):
+    df = raw.rename(
         columns={
             "lifeExp": "life_exp",
             "gdpPercap": "gdp_per_cap",
         },
     )
-    data = data.query("continent == 'Asia'")
-
-    data.to_pickle(produces)
+    return df.query("continent == 'Asia'")
--- src/epp_topics/projects/what_does_pytask_do/screencast/example/task_plot_life_expectancy.py
+++ src/epp_topics/projects/what_does_pytask_do/screencast/example/task_plot_life_expectancy.py
@@ -8,14 +8,18 @@ BLD = Path(__file__).parent / "bld"
 
 
 def task_plot_life_expectancy(
-    data=BLD / "data.pkl",
+    data_file=BLD / "data.pkl",
     produces=BLD / "life_expectancy.svg",
 ):
-    df = pd.read_pickle(data)
-    fig = df.plot(
+    df = pd.read_pickle(data_file)
+    fig = _plot_life_expectancy(df)
+    fig.write_image(produces)
+
+
+def _plot_life_expectancy(df):
+    return df.plot(
         x="year",
         y="life_exp",
         color="country",
         title="Life Expectancy",
     )
-    fig.write_image(produces)
