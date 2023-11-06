2d96c76cd0af5314fa06366b5cb3b87a336c6732
Janos Gabler <janos.gabler@gmail.com>
2023-11-06 08:49:53 +0100

    Add template chapter.

---
+++ src/epp_topics/plotting/config.py
@@ -0,0 +1,29 @@
+"""Definitions of source files for the current chapter."""
+import itertools
+
+from epp_topics.plotting.background import SITE_CONTENTS as BACKGROUND
+
+TOPICS = [
+    BACKGROUND,
+]
+
+
+SITE_CONTENTS = {
+    "chapter_title": "Plotting",
+    "pages": tuple(
+        itertools.chain(
+            ("content_objectives.md",),
+            *[topic["pages"] for topic in TOPICS],
+        ),
+    ),
+    "other": tuple(
+        itertools.chain(
+            *[topic["other"] for topic in TOPICS],
+        ),
+    ),
+    "built": tuple(
+        itertools.chain(
+            *[topic["built"] for topic in TOPICS],
+        ),
+    ),
+}
