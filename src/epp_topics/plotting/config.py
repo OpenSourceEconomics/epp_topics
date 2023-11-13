00e7fa4c5d516b56d1ac63adea0680353c1b1a21
Marcofo21 <115690163+Marcofo21@users.noreply.github.com>
2023-11-13 13:28:44 +0100

    Adds quizzes for tweak_px and corrects config file in plotting.

--- src/epp_topics/plotting/config.py
+++ src/epp_topics/plotting/config.py
@@ -2,13 +2,22 @@
 import itertools
 
 from epp_topics.plotting.goals_workflow import SITE_CONTENTS as GOALS_WORKFLOW
+from epp_topics.plotting.graph_objects import SITE_CONTENTS as GRAPH_OBJECTS
 from epp_topics.plotting.quick_plots_px import SITE_CONTENTS as QUICK_PLOTS_PX
+from epp_topics.plotting.tweak_px import SITE_CONTENTS as TWEAK_PX
 from epp_topics.plotting.what_to_plot import SITE_CONTENTS as WHAT_TO_PLOT
 from epp_topics.plotting.why_plotly_prerequisites import (
     SITE_CONTENTS as WHY_PLOTLY_PREREQUISITES,
 )
 
-TOPICS = [GOALS_WORKFLOW, WHAT_TO_PLOT, WHY_PLOTLY_PREREQUISITES, QUICK_PLOTS_PX]
+TOPICS = [
+    GOALS_WORKFLOW,
+    WHAT_TO_PLOT,
+    WHY_PLOTLY_PREREQUISITES,
+    QUICK_PLOTS_PX,
+    TWEAK_PX,
+    GRAPH_OBJECTS,
+]
 
 
 SITE_CONTENTS = {
