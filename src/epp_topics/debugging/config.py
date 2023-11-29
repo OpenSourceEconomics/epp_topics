f293e4e347d5f9b78a0b3f470069cbb3ad70e3c4
Janos Gabler <janos.gabler@gmail.com>
2023-11-29 11:17:51 +0100

    Add template folders.

--- src/epp_topics/debugging/config.py
+++ src/epp_topics/debugging/config.py
@@ -1,10 +1,24 @@
 """Definitions of source files for the current chapter."""
 import itertools
 
+from epp_topics.debugging.avoiding_debugging import SITE_CONTENTS as AVOIDING_DEBGUGGING
 from epp_topics.debugging.debugging_intro import SITE_CONTENTS as DEBUGGING_INTRO
+from epp_topics.debugging.debugging_psychology import (
+    SITE_CONTENTS as DEBUGGING_PSYCHOLOGY,
+)
+from epp_topics.debugging.debugging_strategies import (
+    SITE_CONTENTS as DEBUGGING_STRATEGIES,
+)
+from epp_topics.debugging.gathering_data import SITE_CONTENTS as GATHERING_DATA
+from epp_topics.debugging.pdbp import SITE_CONTENTS as PDBP
 
 TOPICS = [
     DEBUGGING_INTRO,
+    AVOIDING_DEBGUGGING,
+    DEBUGGING_STRATEGIES,
+    DEBUGGING_PSYCHOLOGY,
+    GATHERING_DATA,
+    PDBP,
 ]
 
 
