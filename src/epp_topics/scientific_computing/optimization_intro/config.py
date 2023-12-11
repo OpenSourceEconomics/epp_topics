4705231a705f0154778f7210a0e9305a4d9f463f
Janos Gabler <janos.gabler@gmail.com>
2023-12-11 11:50:01 +0100

    Add first optimization screencast.

--- src/epp_topics/scientific_computing/optimization_intro/config.py
+++ src/epp_topics/scientific_computing/optimization_intro/config.py
@@ -4,6 +4,7 @@ SITE_CONTENTS = {
     "pages": ("objectives_materials.ipynb",),
     "other": (
         # "existing figures etc.",
+        "screencast/public/sphere.png",
     ),
     "built": ("scientific_computing-optimization_intro.pdf",),
 }
