5411e63977b10c5396e9fba2b0e10ab6f514a0d7
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2024-04-24 12:42:13 +0200

    Replace links.

--- src/epp_topics/scientific_computing/jax_intro/config.py
+++
@@ -1,9 +0,0 @@
-"""Definitions of source files for the current chapter."""
-
-SITE_CONTENTS = {
-    "pages": ("objectives_materials.ipynb",),
-    "other": (
-        # "existing figures etc.",
-    ),
-    "built": ("scientific_computing-jax_intro.pdf",),
-}
