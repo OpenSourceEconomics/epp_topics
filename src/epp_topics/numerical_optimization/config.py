4c012579957edc5efdd5f65fd77460fe4e64789e
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2025-01-09 13:21:56 +0100

    Fix obvious errors.

--- src/epp_topics/numerical_optimization/config.py
+++ src/epp_topics/numerical_optimization/config.py
@@ -2,43 +2,47 @@
 
 import itertools
 
-from epp_topics.scientific_computing.db_line_search import (
+from epp_topics.numerical_optimization.db_line_search import (
     SITE_CONTENTS as DB_LINE_SEARCH,
 )
-from epp_topics.scientific_computing.db_trust_region import (
+from epp_topics.numerical_optimization.db_trust_region import (
     SITE_CONTENTS as DB_TRUST_REGION,
 )
-from epp_topics.scientific_computing.df_direct_search import (
+from epp_topics.numerical_optimization.df_direct_search import (
     SITE_CONTENTS as DF_DIRECT_SEARCH,
 )
-from epp_topics.scientific_computing.df_trust_region import (
+from epp_topics.numerical_optimization.df_trust_region import (
     SITE_CONTENTS as DF_TRUST_REGION,
 )
-from epp_topics.scientific_computing.optimagic_overview import (
+from epp_topics.numerical_optimization.grid_search import (
+    SITE_CONTENTS as GRID_SEARCH,
+)
+from epp_topics.numerical_optimization.optimagic_overview import (
     SITE_CONTENTS as OPTIMAGIC_OVERVIEW,
 )
-from epp_topics.scientific_computing.optimization_algorithms import (
+from epp_topics.numerical_optimization.optimization_algorithms import (
     SITE_CONTENTS as OPTIMIZATION_ALGORITHMS,
 )
-from epp_topics.scientific_computing.optimization_histories import (
+from epp_topics.numerical_optimization.optimization_histories import (
     SITE_CONTENTS as OPTIMIZATION_HISTORIES,
 )
-from epp_topics.scientific_computing.optimization_intro import (
+from epp_topics.numerical_optimization.optimization_intro import (
     SITE_CONTENTS as OPTIMIZATION_INTRO,
 )
-from epp_topics.scientific_computing.optimization_mechanics import (
+from epp_topics.numerical_optimization.optimization_mechanics import (
     SITE_CONTENTS as OPTIMIZATION_MECHANICS,
 )
-from epp_topics.scientific_computing.set_up_function_grid_search import (
-    SITE_CONTENTS as SET_UP_FUNCTION_GRID_SEARCH,
+from epp_topics.numerical_optimization.set_up_function import (
+    SITE_CONTENTS as SET_UP_FUNCTION,
 )
 
 TOPICS = [
-    SET_UP_FUNCTION_GRID_SEARCH,
     OPTIMIZATION_INTRO,
     OPTIMIZATION_MECHANICS,
     OPTIMAGIC_OVERVIEW,
     OPTIMIZATION_ALGORITHMS,
+    SET_UP_FUNCTION,
+    GRID_SEARCH,
     DB_LINE_SEARCH,
     DB_TRUST_REGION,
     DF_DIRECT_SEARCH,
