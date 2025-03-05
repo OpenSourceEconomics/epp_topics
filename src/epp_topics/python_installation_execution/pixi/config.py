f921bf1b2425d2262e3479dea741608df39f7083
Tim Mensinger <mensingertim@gmail.com>
2025-03-05 17:42:06 +0100

    Add pixi sub-chapter

---
+++ src/epp_topics/python_installation_execution/pixi/config.py
@@ -0,0 +1,9 @@
+"""Definitions of source files for the current chapter."""
+
+SITE_CONTENTS = {
+    "pages": ("objectives_materials.ipynb",),
+    "other": (
+        # "existing figures etc.",
+    ),
+    "built": ("python_installation_execution-pixi_intro.pdf",),
+}
